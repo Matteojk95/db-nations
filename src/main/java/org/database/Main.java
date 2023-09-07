d72c90e1862a78b281c89e697c5aa0402fa33bcd
Matteojk95 <matteopalmer2095@gmail.com>
2023-09-07 16:57:46 +0200

    Milestone 3 dovrebbe essere finito però ho problemi con i controlli su maven e non riesco a fare i test purtroppo

--- src/main/java/org/database/Main.java
+++ src/main/java/org/database/Main.java
@@ -1,11 +1,12 @@
 package org.database;
 
 import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-
+        Scanner scan = new Scanner(System.in);
         String URL = "jdbc:mysql://localhost: 3306/ Db_nations";
         String username = "root";
         String password= "";
@@ -16,7 +17,6 @@ public class Main {
         "JOIN regions ON countries.region_id = regions.region_id" +
         "JOIN continents ON regions.continent_id = continents.continent_id" +
         "ORDER BY countries.name" ;
-
 
 
         try (Connection connection = DriverManager.getConnection(URL, username, password)) {
@@ -37,6 +37,22 @@ public class Main {
                     }
                 }
             }
+
+            //chiedo all'utente di inserire una stringa
+            System.out.println("inserisci una stringa");
+            String stringadiricerca =  scan.nextLine() ;
+            System.out.println("you choose" + stringadiricerca);
+            String query = "SELECT countries.name AS country_name, country_id AS country_id, regions.name  AS regions_name, continents.name AS continents_name" +
+                    "FROM countries" +
+                    "JOIN regions ON countries.region_id = regions.region_id" +
+                    "JOIN continents ON regions.continent_id = continents.continent_id" +
+                    "ORDER BY countries.name" + "FROM countries where name like ?";
+            try(PreparedStatement ps = connection.prepareStatement(query)){
+                ps.setString(1,"%"+ stringadiricerca + "%");
+            }
+
+
+
 
           }  catch (SQLException e) {
              System.out.println("c'è un errore");
